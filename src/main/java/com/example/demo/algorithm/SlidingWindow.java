09fc3e8c6a71c3beadd786f70c5b618baad7e980
zeti <zmk95511@163.com>
2020-09-26 18:52:29 +0800

    算法-滑动窗口

---
+++ src/main/java/com/example/demo/algorithm/SlidingWindow.java
@@ -0,0 +1,55 @@
+package com.example.demo.algorithm;
+
+/**
+ * Description:  滑动窗口算法
+ * <p>
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ *
+ * @author Zeti
+ * @date 2020/9/26 6:48 下午
+ */
+public class SlidingWindow {
+
+    public static void main(String[] args) {
+
+        int s = 8;
+
+        int[] nums = {4, 3, 3, 2, 1, 4, 0, 6};
+
+        int[] a = {2, 3, 1, 2, 4, 3};
+
+        int i = minSubArrayLen(7, a);
+
+        System.err.println("最小数组长度为：" + i);
+
+    }
+
+    public static int minSubArrayLen(int s, int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return nums[0] == s ? 1 : 0;
+
+        int left = 0;
+        int right = 0;
+        int window = 0;
+        int res = Integer.MAX_VALUE;
+        while (right < len) {
+            for (; right < len && window < s; right++)
+                window += nums[right];
+
+
+            while (window >= s) {
+                res = Math.min(res, right - left);
+
+                window -= nums[left];
+                left++;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+}
