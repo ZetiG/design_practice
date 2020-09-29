36e97942a541040a3af7c8bc434e0ac13cd2ee15
zeti <zmk95511@163.com>
2020-09-29 15:23:53 +0800

    quick sort init

---
+++ src/main/java/com/example/demo/algorithm/QuickSort.java
@@ -0,0 +1,29 @@
+package com.example.demo.algorithm;
+
+
+/**
+ * Description: 快排算法
+ *
+ * @author Zeti
+ * @date 2020/9/28 6:29 下午
+ */
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, 6, 4, 1, 8, 0, 4, 1};
+
+    }
+
+
+    private static int[] quickSortDemo(int[] arr) {
+
+        if (arr == null || arr.length <= 0) {
+            return new int[]{};
+        }
+
+
+        return null;
+    }
+}
