b83c19772c8a778ddb92804e64de7c85efc16093
zeti <zmk95511@163.com>
2021-03-03 19:42:05 +0800

    适配器模式

---
+++ src/main/java/com/example/demo/behavior_type/adapter/Adaptee.java
@@ -0,0 +1,16 @@
+package com.example.demo.behavior_type.adapter;
+
+/**
+ * Description: 需要适配的类
+ *
+ * @author Zeti
+ * @date 2021/3/3 下午7:33
+ */
+public class Adaptee {
+
+
+    public void adaptecMethod() {
+        System.out.println("适配方法。。。");
+    }
+
+}
