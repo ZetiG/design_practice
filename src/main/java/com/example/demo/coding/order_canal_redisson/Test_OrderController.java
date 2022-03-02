ef6d851d4cdf0b7bf3b4b6421b07410f110e5bdb
zeti <zmk95511@163.com>
2022-03-02 16:29:21 +0800

    upd

--- src/main/java/com/example/demo/coding/order_canal_redisson/Test_OrderController.java
+++ src/main/java/com/example/demo/coding/order_canal_redisson/Test_OrderController.java
@@ -4,7 +4,6 @@ import org.redisson.api.RBlockingQueue;
 import org.redisson.api.RDelayedQueue;
 import org.redisson.api.RedissonClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
