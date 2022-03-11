34668a60b298fa143dd7a66be032f9482a3a2784
zeti <zmk95511@163.com>
2022-03-11 18:08:56 +0800

    数据结构-跳跃表初步实现

--- src/main/java/com/example/demo/data_structure/SkipList.java
+++ src/main/java/com/example/demo/data_structure/SkipList.java
@@ -48,7 +48,7 @@ public class SkipList<T> {
 
             // 右节点即为目标
             if (head.right.idx == target) {
-                return head;
+                return head.right;
             } else {
                 head = head.right;
             }
@@ -68,61 +68,67 @@ public class SkipList<T> {
             return;
         }
 
-        //
-        Stack<Node<T>> stack = new Stack();
-        Node head = this.head;
+        // 定义栈，记录需要建立的索引
+        Stack<Node<T>> stack = new Stack<>();
+        Node<T> head = this.head;
         while (head != null) {
 
-            // 右侧无节点 或者 右侧节点大于当前插入节点，向下
-            if (head.right == null || head.right.idx > idx) {
-
-                stack.add(head);//将曾经向下的节点记录一下
+            // 当前节点等于目标节点 或者 右侧无节点 或者 右侧节点大于当前插入节点，向下
+            if (head.idx == idx || head.right == null || head.right.idx > idx) {
+                // 将曾经向下的节点记录一下
+                stack.add(head);
                 head = head.down;
-                continue;
 
             } else {
-
                 // 右侧有节点，且小于当前节点，则继续向右查找
                 head = head.right;
             }
+        }
 
-            // 当前索引层，初始第一层
-            int level = 1;
-            Node downNode = null;
+        // 当前索引层，初始第一层
+        int level = 1;
+        Node<T> downNode = null;
 
-            while (!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
 
-                //在该层插入node
-                head = stack.pop();//抛出待插入的左侧节点
-                Node nodeTeam = new Node(node.idx, node.val);//节点需要重新创建
-                nodeTeam.down = downNode;//处理竖方向
-                downNode = nodeTeam;//标记新的节点下次使用
-                if (head.right == null) {//右侧为null 说明插入在末尾
-                    head.right = nodeTeam;
-                }
-                //水平方向处理
-                else {//右侧还有节点，插入在两者之间
-                    nodeTeam.right = head.right;
-                    head.right = nodeTeam;
-                }
-                //考虑是否需要向上
-                if (level > MAX_LEVEL)//已经到达最高级的节点啦
-                    break;
-                double num = RandomUtils.nextDouble();//[0-1]随机数
-                if (num > 0.5)//运气不好结束
-                    break;
-                level++;
-                if (level > highLevel)//比当前最大高度要高但是依然在允许范围内 需要改变head节点
-                {
-                    highLevel = level;
-                    //需要创建一个新的节点
-                    Node highHeadNode = new Node(Integer.MIN_VALUE, null);
-                    highHeadNode.down = head;
-                    head = highHeadNode;//改变head
-                    stack.add(head);//下次抛出head
-                }
+            // 抛出待插入的左侧节点, 并在该层插入node
+            head = stack.pop();
+
+            Node<T> nodeTeam = new Node<>(node.idx, node.val);//节点需要重新创建
+            nodeTeam.down = downNode;   // 处理竖方向
+            downNode = nodeTeam;    // 标记新的节点下次使用
+
+            //右侧为null 说明插入在末尾
+            if (head.right == null) {
+                head.right = nodeTeam;
+
+            } else if (head.right.idx > node.idx) {    // 右侧还有节点，插入在两者之间
+                nodeTeam.right = head.right;
+                head.right = nodeTeam;
+            }
+
+            // 判断是否需要结束创建索引，一、到达最高级节点；二、随机数概率
+            if (level > MAX_LEVEL || RandomUtils.nextFloat(0,1) > 0.7) {
+                break;
+            }
+
+            // 向上建索引
+            level++;
+
+            // 比当前最大高度要高但是依然在允许范围内 需要改变head节点
+            if (level > highLevel) {
+                highLevel = level;
+                //需要创建一个新的节点
+                Node<T> highHeadNode = new Node<>(Integer.MIN_VALUE, null);
+//                highHeadNode.down = nodeTeam;
+                highHeadNode.down = this.head;
+                this.head = highHeadNode;
+                // TODO: 2022/3/11  
+                head = highHeadNode;    // 改变head
+                stack.add(head);    // 下次抛出head
             }
         }
+
     }
 
     /**
@@ -182,13 +188,11 @@ public class SkipList<T> {
             Node enumLast=last.right;
             System.out.printf("%-8s","head->");
             while (enumLast!=null&&enumNode!=null) {
-                if(enumLast.idx==enumNode.idx)
-                {
-                    System.out.printf("%-5s",enumLast.idx+"->");
+                if(enumLast.idx==enumNode.idx) {
+                    System.out.printf("%-5s",enumLast.idx+"->" + enumLast.val + "  ");
                     enumLast=enumLast.right;
                     enumNode=enumNode.right;
-                }
-                else{
+                } else{
                     enumLast=enumLast.right;
                     System.out.printf("%-5s","");
                 }
@@ -199,11 +203,20 @@ public class SkipList<T> {
             System.out.println();
         }
     }
+
+    //  1       5
+    //  1   3   5   7
+    //  1 2 3 4 5 6 7 8
     public static void main(String[] args) {
         SkipList<Integer>list=new SkipList<>();
-        for(int i=1;i<20;i++) {
+        for(int i=1;i<30;i++) {
+            i++;
             list.add(new Node(i,i + "H"));
         }
+        list.printList();
+
+
+        list.add(new Node<>(5, 55));
 
         list.printList();
 //        list.erase(4);
