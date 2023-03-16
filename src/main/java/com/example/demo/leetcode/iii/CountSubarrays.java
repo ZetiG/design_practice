51a917bc7de41dc82e7612bdc9152b76c6ec08f5
zeti <zmk95511@163.com>
2023-03-16 20:02:58 +0800

    lc-2488. 统计中位数为 K 的子数组-转换值+hash表

--- src/main/java/com/example/demo/leetcode/iii/CountSubarrays.java
+++ src/main/java/com/example/demo/leetcode/iii/CountSubarrays.java
@@ -2,6 +2,7 @@ package com.example.demo.leetcode.iii;
 
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Description: 2488. 统计中位数为 K 的子数组
@@ -36,10 +37,33 @@ public class CountSubarrays {
 
     }
 
+    public static int countSubarrays(int[] nums, int k) {
+        int pos = 0, n = nums.length;
+        while (nums[pos] != k) {
+            ++pos;
+        }
+
+        var cnt = new HashMap<Integer, Integer>();
+        // i=pos 的时候 x 是 0，直接记到 cnt 中，这样下面不是大于 k 就是小于 k
+        cnt.put(0, 1);
+        for (int i = pos - 1, x = 0; i >= 0; --i) { // 从 pos-1 开始累加 x
+            x += nums[i] < k ? 1 : -1;
+            cnt.merge(x, 1, Integer::sum);
+        }
+
+        // i=pos 的时候 x 是 0，直接加到答案中，这样下面不是大于 k 就是小于 k
+        int ans = cnt.get(0) + cnt.getOrDefault(-1, 0);
+        for (int i = pos + 1, x = 0; i < n; ++i) { // 从 pos+1 开始累加 x
+            x += nums[i] > k ? 1 : -1;
+            ans += cnt.getOrDefault(x, 0) + cnt.getOrDefault(x - 1, 0);
+        }
+        return ans;
+    }
+
     // 暴力解法，TLE超时
     // nums = [3,2,1,4,5], k = 4
     //解释：中位数等于 4 的子数组有：[4]、[4,5] 和 [1,4,5] 。
-    public static int countSubarrays(int[] nums, int k) {
+    public static int countSubarrays2(int[] nums, int k) {
         int total = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i; j < nums.length; j++) {
