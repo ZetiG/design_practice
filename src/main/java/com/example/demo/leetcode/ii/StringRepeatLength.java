7dfba69411146ba05fd10699d963a0ef1621dd14
zeti <zmk95511@163.com>
2024-04-28 15:46:36 +0800

    1883. 准时抵达会议现场的最小跳过休息次数

--- src/main/java/com/example/demo/leetcode/ii/StringRepeatLength.java
+++ src/main/java/com/example/demo/leetcode/ii/StringRepeatLength.java
@@ -1,5 +1,6 @@
 package com.example.demo.leetcode.ii;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -47,14 +48,11 @@ public class StringRepeatLength {
     public static int lengthOfLongestSubstring(String s) {
         // 记录字符上一次出现的位置
         int[] last = new int[128];
-        for (int i = 0; i < 128; i++) {
-            last[i] = -1;
-        }
-        int n = s.length();
+        Arrays.fill(last, -1);
 
         int res = 0;
         int start = 0; // 窗口开始位置
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             int index = s.charAt(i);
             start = Math.max(start, last[index] + 1);
             res = Math.max(res, i - start + 1);
