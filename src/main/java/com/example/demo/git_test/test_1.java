b2387f08f2a699f4448b7640fad7a62923629dbc
zeti <zmk95511@163.com>
2024-10-19 11:52:02 +0800

    t22

--- src/main/java/com/example/demo/git_test/test_1.java
+++ src/main/java/com/example/demo/git_test/test_1.java
@@ -7,7 +7,7 @@ package com.example.demo.git_test;
  */
 public class test_1 {
     private static String t1 = "t1";
-    private static String t2 = "t2";
+    private static String t2 = "t22";
     public static void main(String[] args) {
         System.err.println(t2);
     }
